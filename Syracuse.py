47d1454edfd098af32c5b42fda6adc35cdc9b06f
Bertolelli <bertolelli@et.esiea.fr>
2022-03-04 13:52:27 +0100

    TP1

--- Syracuse.py
+++ Syracuse.py
@@ -1,10 +1,10 @@
-print("Entrer un entier strictement positif : ")
+print("Entrer un entier strictement positif : ", end="")
 var = int(input())
-L = [float(var)]
+L = [var]
 while var !=1:
     if var%2==0:
-        var = var/2
-        L.append(var)
+        var = var//2
+        L.append(int(var))
     elif var%2!=0:
         var = var*3+1
         L.append(var)
