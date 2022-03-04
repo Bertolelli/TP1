5d99d78b5f756ed93438ce094e86e339fdc3d29b
Bertolelli <bertolelli@et.esiea.fr>
2022-03-04 13:08:00 +0100

    TP1

--- Syracuse_IO.py
+++ Syracuse_IO.py
@@ -1,25 +1,51 @@
+class Cfile(object):
+    def __init__(self, fn):
+        self.fn = fn
+    def wri(self, msg):
+        with open(self.fn, 'a') as myFile :
+            myFile.write(msg)
+    def rea(self):
+        with open(self.fn, 'r') as readFile :
+            rf = readFile.read()
+            print(rf)
+    def clo(self):
+        with open(self.fn) as closeFile :
+            closeFile.close()
+    def rl(self):
+        with open(self.fn, 'r') as readLine :
+            rel = readLine.readlines()
+            if rel is not None:
+                return 1
+            # print(rel)
+    def dele(self, msg):
+        with open(self.fn, 'w') as deleteContent:
+            deleteContent.write(msg)
+
+cf = Cfile('Ret.txt')
+re = cf.rl()
+if re == 1:
+    cf.dele("")
+
 print("Entrez un entier strictement positif : ", end="")
 var = int(input())
 L = [float(var)]
-f = open("Ret.txt", "w")
 var2 = float(var)
-f.write(str(var2))
-f.write(", ")
-while var !=1:
+cf.wri(str(var2))
+cf.wri(", ")
+while var != 1:
     if var%2==0:
         var = var/2
         L.append(var)
-        f.write(str(var))
+        cf.wri(str(var))
         if var != 1:
-            f.write(", ")
-    elif var%2!=0:
-        var = var*3+1
+            cf.wri(", ")
+    elif var%2 != 0:
+        var =  var*3+1
         L.append(var)
-        f.write(str(var))
+        cf.wri(str(var))
         if var != 1:
-            f.write(", ")
-f.close()
-F = open("Ret.txt", "r")
-print(L)
-print(F.read())
-F.close()
+            cf.wri(", ")
+
+print("\n", L, "\n")
+cf.rea()
+cf.clo()
